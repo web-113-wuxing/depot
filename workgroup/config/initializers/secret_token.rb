5de7291615c8f720a83f0dfaf00c64a74aa79df8
wangshuai427 <244580440@qq.com>
2011-08-01 09:36:12 +0800

    workgroup

---
+++ workgroup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7500aaaf1417d87a4829f4dc3c78a6d544758b7667f31dbe29dc39ec83ee7dd3ef691041803ca3c5b31321d5e28606452f7a88fe5cc7ae4bdc80fd1b57c78b40'
