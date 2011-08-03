826f5d5cba463d2480cb29af606c91f316815b16
wugangbao <444075191@qq.com>
2011-08-03 11:48:55 +0800

    wugang

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,18 +1,18 @@
 class Cart < ActiveRecord::Base
-  has_many :line_item, :dependent => :destroy
+  has_many :line_items, :dependent => :destroy
   def add_product(product_id)
-    current_item = line_item.where(:product_id => product_id).first
+    current_item = line_items.where(:product_id => product_id).first
     if current_item
        current_item.quantity += 1
      else
        current_item = LineItem.new(:product_id=>product_id)
-       line_item << current_item
+       line_items<< current_item
        
     end
     current_item
   end
   
   def total_price
-    line_item.to_a.sum{|item| item.total_price}
+    line_items.to_a.sum{|item| item.total_price}
   end
 end
